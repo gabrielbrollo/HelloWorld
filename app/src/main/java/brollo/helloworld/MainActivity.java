2628b315692cf7d8c83d275d779a48952244be01
Gabriel Brollo <gabrielbrollo@sparkag.com.br>
2018-06-05 14:13:41 -0300

    teste

--- app/src/main/java/brollo/helloworld/MainActivity.java
+++ app/src/main/java/brollo/helloworld/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);f
         setContentView(R.layout.activity_main);
         System.out.println(""d);
     }
